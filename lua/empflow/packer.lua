4750716a5b95227d9686c2576e4074b5cc7544d7
Matvey <empflow@tutanota.com>
2023-07-04 12:59:35 +0300

    add lualine

--- lua/empflow/packer.lua
+++ lua/empflow/packer.lua
@@ -4,43 +4,47 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.2',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-	use {
-		'sainnhe/sonokai',
-		config = function()
-			vim.cmd('colorscheme sonokai')
-		end
-	}
+    use {
+        'sainnhe/sonokai',
+        config = function()
+            vim.cmd('colorscheme sonokai')
+        end
+    }
 
-	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-	use('nvim-treesitter/playground');
-	use('theprimeagen/harpoon')
-	use('mbbill/undotree')
-	use('tpope/vim-fugitive')
-	use({
-	    'VonHeikemen/lsp-zero.nvim',
-	    branch = 'v2.x',
-	    requires = {
-	      -- LSP Support
-	      {'neovim/nvim-lspconfig'},             -- Required
-	      {                                      -- Optional
-		'williamboman/mason.nvim',
-		build = function()
-		  pcall(vim.cmd, 'MasonUpdate')
-		end,
-	      },
-	      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-treesitter/playground');
+    use('theprimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+    use({
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {                     -- Optional
+                'williamboman/mason.nvim',
+                build = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-	      -- Autocompletion
-	      {'hrsh7th/nvim-cmp'},     -- Required
-	      {'hrsh7th/cmp-nvim-lsp'}, -- Required
-	      {'L3MON4D3/LuaSnip'},     -- Required
-	    }
-	  })
-  end)
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' }, -- Required
+        }
+    })
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+end)
