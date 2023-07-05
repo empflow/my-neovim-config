9f0064a70c03b03fb3e1a54d1e97e284391bcbfc
Matvey <empflow@tutanota.com>
2023-07-05 15:12:39 +0300

    install nvim-autopairs

--- lua/empflow/packer.lua
+++ lua/empflow/packer.lua
@@ -4,60 +4,64 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
+  use 'wbthomason/packer.nvim'
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.2',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.2',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
 
-    use {
-        'sainnhe/sonokai',
-        config = function()
-            vim.cmd('colorscheme sonokai')
-        end
-    }
+  use {
+    'sainnhe/sonokai',
+    config = function()
+      vim.cmd('colorscheme sonokai')
+    end
+  }
 
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-    use('nvim-treesitter/playground');
-    use('theprimeagen/harpoon')
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
-    use({
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' }, -- Required
-            {                            -- Optional
-                'williamboman/mason.nvim',
-                build = function()
-                    pcall(vim.cmd, 'MasonUpdate')
-                end,
-            },
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+  use('nvim-treesitter/playground');
+  use('theprimeagen/harpoon')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+  use({
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },       -- Required
+      {                                  -- Optional
+        'williamboman/mason.nvim',
+        build = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      { 'williamboman/mason-lspconfig.nvim' },       -- Optional
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },     -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'L3MON4D3/LuaSnip' },     -- Required
-        }
-    })
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },           -- Required
+      { 'hrsh7th/cmp-nvim-lsp' },       -- Required
+      { 'L3MON4D3/LuaSnip' },           -- Required
     }
-    use("theprimeagen/vim-be-good")
-    use {
-        'unblevable/quick-scope',
-        setup = function()
-            vim.g.qs_highlight_on_keys = { 'f', 'F', 't', 'T' }
-        end
-    }
-    use {
-        'nvim-tree/nvim-tree.lua',
-    }
-    use('kyazdani42/nvim-web-devicons')
-    use("tpope/vim-surround")
-    use { "akinsho/toggleterm.nvim", tag = '*' }
+  })
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+  use("theprimeagen/vim-be-good")
+  use {
+    'unblevable/quick-scope',
+    setup = function()
+      vim.g.qs_highlight_on_keys = { 'f', 'F', 't', 'T' }
+    end
+  }
+  use {
+    'nvim-tree/nvim-tree.lua',
+  }
+  use('kyazdani42/nvim-web-devicons')
+  use("tpope/vim-surround")
+  use { "akinsho/toggleterm.nvim", tag = '*' }
+  use {
+    "windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+  }
 end)
