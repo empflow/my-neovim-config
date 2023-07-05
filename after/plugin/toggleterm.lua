120988382c035b3e874a06d27f620b5b235a147e
Matvey <empflow@tutanota.com>
2023-07-05 17:53:42 +0300

    change terminal size

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,11 +1,12 @@
 require("toggleterm").setup({
-   close_on_exit = true,
-   open_mapping = [[<C-\>]]
+  close_on_exit = true,
+  open_mapping = [[<C-\>]],
+  size = 30
 })
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 function _G.set_terminal_keymaps()
-  local opts = {buffer = 0}
+  local opts = { buffer = 0 }
   vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts) -- use normal mode
 end
